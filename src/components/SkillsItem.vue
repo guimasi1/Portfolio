db70e16fc71bd65ee08f94bb74af8242435a8f66
guimasi1 <143355576+guimasi1@users.noreply.github.com>
2024-10-09 11:15:21 +0200

    add python icon to skills set

--- src/components/SkillsItem.vue
+++ src/components/SkillsItem.vue
@@ -66,6 +66,13 @@
           <div class="flex align-middle justify-center hover:scale-125 duration-200">
             <img class="w-2/3" src="../assets/Figma-logo.svg" alt="" />
           </div>
+          <div class="flex align-middle justify-center hover:scale-125 duration-200">
+            <img
+              class="w-60"
+              src="https://raw.githubusercontent.com/devicons/devicon/ca28c779441053191ff11710fe24a9e6c23690d6/icons/python/python-original.svg"
+              alt=""
+            />
+          </div>
         </div>
       </div>
     </div>
